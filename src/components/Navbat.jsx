37a89cfb911ed5663ca7495a665b8a3f19362521
FilmyFather <91774433+FilmyFather@users.noreply.github.com>
2025-03-20 00:04:59 +0530

    Update Navbat.jsx

--- src/components/Navbat.jsx
+++ src/components/Navbat.jsx
@@ -1,1 +1,11 @@
- 
+ import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="bg-blue-600 p-4 text-white text-center text-xl font-bold">
+            Yuvraj Color Hack
+        </nav>
+    );
+};
+
+export default Navbar;
